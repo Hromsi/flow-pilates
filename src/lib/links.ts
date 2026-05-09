bfd195a184608f9b2b19f71fa16ed705ce568702
hromsi <bodak2011@yandex.ru>
2026-05-09 11:48:32 +0300

    feat: update maxLink to point to the new join URL
    - Changed maxLink from a placeholder to the actual join URL for improved functionality.
    - This update ensures users are directed to the correct resource when accessing the link.

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,7 +1,7 @@
 export const bookingLink =
   "https://n1730823.yclients.com/company/1543386/about?o=m4492392&previousStepUrl=%2Fcompany%2F1543386%2Frecord-type%3Fo%3Dm4492392";
 export const telegramLink = "https://t.me/+fQooG2dMMcc4NmRi";
-export const maxLink = "#";
+export const maxLink = "https://max.ru/join/OFrffvE_r0F1JnYqPxGbD3yKM-lp871YxqzL-whuRpc";
 export const phoneLink = "tel:+79857148630";
 export const mailLink = "mailto:Keit-Obninsk@yandex.ru";
 export const mapsLink =
